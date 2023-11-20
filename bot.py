da21e31dae6d9411720b5f3d6d8a0741c699bace
Kevin CUOQ <kevin.cuoq@gmail.com>
2023-11-20 19:18:51 +0100

    Add summary text at the end of the process

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import aiohttp
+import asyncio
+from slugify import slugify
 
 import yt_dlp
 
@@ -18,95 +20,179 @@ DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
-async def downloadByUrl(ctx, url):
-	alldebrid_client = alldebrid.AllDebrid()
-	try:
-		data = alldebrid_client.debrid_link(url)
-	except AssertionError as e:
-		await ctx.send(f'Error: {e}')
-		return
-	
-	filename = data['filename']
-	
-	async with aiohttp.ClientSession() as session:
-		async with session.get(data['link']) as resp:
-			if resp.status != 200:
-				await ctx.send(f'Error: {resp.status}')
-				return
-			with open(os.path.join(DOWNLOAD_PATH, filename), 'wb') as f:
-				while True:
-					chunk = await resp.content.read(1024)
-					if not chunk:
-						break
-					f.write(chunk)
+async def download_by_url(ctx, url, type):
+    alldebrid_client = alldebrid.AllDebrid()
+    try:
+        data = alldebrid_client.debrid_link(url)
+    except AssertionError as e:
+        await ctx.send(f'Error: {e}')
+        return False
+    
+    filename = data['filename']
+    await ctx.send(f'Debrid {filename} / {data["link"]}')
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(data['link']) as resp:
+            if resp.status != 200:
+                await ctx.send(f'Error: {resp.status}')
+                return False
+            
+            if type == 'movie':
+                path = os.path.join(DOWNLOAD_PATH, 'Movies')
+            elif type == 'serie':
+                path = os.path.join(DOWNLOAD_PATH, 'Shows')
+            else:
+                return False
+            
+            with open(os.path.join(path, filename), 'wb') as f:
+                while True:
+                    chunk = await resp.content.read(1024)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+    
+    return filename
 
 
 @bot.command(name='music', help="Download a music file")
 async def music(ctx, url):
-	if not (url.startswith('https://')):
-		await ctx.send('Invalid url')
-		return
-	
-	with yt_dlp.YoutubeDL({
-		'extract_audio': True, 
-		'format': 'bestaudio', 
-		'outtmpl': '/home/gones/Musique/%(title)s.mp3'
-	}) as video:
-		info_dict = video.extract_info(url, download = True)
-		video_title = info_dict['title']
-
-		await ctx.send(f'You want to download: {url}')
-
-		video.download(url)
-
-		await ctx.send(f'Download finished')
+    if not (url.startswith('https://')):
+        await ctx.send('Invalid url')
+        return
+    
+    with yt_dlp.YoutubeDL(
+        {'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '/home/gones/Musique/%(title)s.mp3'}) as video:
+        info_dict = video.extract_info(url, download=True)
+        video_title = info_dict['title']
+        
+        await ctx.send(f'You want to download: {url}')
+        
+        video.download(url)
+        
+        await ctx.send(f'Download finished')
 
 
 @bot.command(name='download', help='Download a file')
 async def download(ctx, url):
-	if not (url.startswith('http://') or url.startswith('https://')):
-		await ctx.send('Invalid url')
-		return
-
-	await ctx.send(f'You want to download: {url}')
-
-	alldebrid_client = alldebrid.AllDebrid()
-	try:
-		data = alldebrid_client.debrid_link(url)
-	except AssertionError as e:
-		await ctx.send(f'Error: {e}')
-		return
-
-	filename = data['filename']
-
-	async with aiohttp.ClientSession() as session:
-		async with session.get(data['link']) as resp:
-			if resp.status != 200:
-				await ctx.send(f'Error: {resp.status}')
-				return
-			with open(os.path.join(DOWNLOAD_PATH, filename), 'wb') as f:
-				while True:
-					chunk = await resp.content.read(1024)
-					if not chunk:
-						break
-					f.write(chunk)
-
-	await ctx.send(f'Download finished: {filename}')
+    if not (url.startswith('http://') or url.startswith('https://')):
+        await ctx.send('Invalid url')
+        return
+    
+    await ctx.send(f'You want to download: {url}')
+    
+    alldebrid_client = alldebrid.AllDebrid()
+    try:
+        data = alldebrid_client.debrid_link(url)
+    except AssertionError as e:
+        await ctx.send(f'Error: {e}')
+        return
+    
+    filename = data['filename']
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(data['link']) as resp:
+            if resp.status != 200:
+                await ctx.send(f'Error: {resp.status}')
+                return
+            with open(os.path.join(DOWNLOAD_PATH, filename), 'wb') as f:
+                while True:
+                    chunk = await resp.content.read(1024)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+    
+    await ctx.send(f'Download finished: {filename}')
 
 
 @bot.command(name='series', help='Download all episodes of a series')
 async def series(ctx, url):
-	if not (url.startswith('http://') or url.startswith('https://')):
-		await ctx.send('Invalid url')
-		return
+    if not (url.startswith('http://') or url.startswith('https://')):
+        await ctx.send('Invalid url')
+        return
+    
+    await ctx.send(f'You want to download: {url}')
+    
+    parser = Parser()
+    urls = parser.download_all_series(url)
+    for url in urls:
+        await download_by_url(ctx, url)
 
-	await ctx.send(f'You want to download: {url}')
-	
-	parser = Parser(ctx)
-	urls = parser.download_all_series(url)
-	for url in urls:
-		await downloadByUrl(ctx, url)
-	
-	parser.driver.close()
+
+@bot.command(name='wawacity', help='Download a movie')
+async def wawacity(ctx, type, url):
+    if type is None and url is None:
+        await ctx.send('Invalid arguments')
+        return
+    
+    if not (url.startswith('http://') or url.startswith('https://')):
+        await ctx.send('Invalid url')
+        return
+    
+    if not (type == 'movie' or type == 'serie'):
+        await ctx.send('Invalid type')
+        return
+    
+    parser = Parser(show_logs=True)
+
+    title, urls = parser.get_dl_protect_url(url)
+    if not urls:
+        await ctx.send(f'Error: No link found')
+        return
+    
+    await ctx.send(f'You want to download: {title}')
+    print(urls)
+    
+    loop = asyncio.get_event_loop()
+    summary = []
+    
+    for url in urls:
+        print(f'You want to find url from {url}')
+        tries = 0
+        dl_protect_url = None
+        error = None
+        while tries < 3 and not dl_protect_url:
+            try:
+                future = loop.run_in_executor(None, parser.dl_protect, url)
+                dl_protect_url = await future
+            except Exception as e:
+                tries += 1
+                print(f"Tries {tries}/3: {e}")
+                future = loop.run_in_executor(None, parser.dl_protect, url)
+                dl_protect_url = await future
+
+        print(f"Tries {tries}/3 - {dl_protect_url}")
+        if tries == 3 and not dl_protect_url:
+            download_status = 'ERROR'
+            error = 'Too many tries'
+        else:
+            if dl_protect_url:
+                try:
+                    print(f'You want to download: {dl_protect_url}')
+                    title = await download_by_url(ctx, dl_protect_url, type)
+                    if title:
+                        download_status = 'OK'
+                    else :
+                        download_status = 'ERROR'
+                        error = 'Download error'
+                except Exception as e:
+                    download_status = 'ERROR'
+                    error = e
+            else:
+                download_status = 'ERROR'
+                error = 'No link found'
+
+        summary.append({
+            'title': title,
+            'url': url,
+            'dl_protect_url': dl_protect_url,
+            'download_status': download_status,
+            'error': error
+        })
+        
+    with open(f'summary/{slugify(title)}.txt', 'w') as f:
+        for item in summary:
+            f.write(f"URL: {item['url']}, DL Protect URL: {item['dl_protect_url']}, Download Status: {item['download_status']}")
+            
+    await ctx.send(f"Summary: {len(summary)} files downloaded")
 
 bot.run(TOKEN)
--- parser.py
+++ parser.py
@@ -5,27 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
 from seleniumbase import Driver, SB
-from seleniumbase.fixtures.page_actions import is_element_enabled
 
 XPATH_LINK = '//*[@id="protected-container"]/div[2]/div/ul/li/a'
 SELECT_PROVIDER = '1fichier'
 
 
 class Parser:
-    def __init__(self, ctx=None, show_logs=False):
-        self.ctx = ctx
+    def __init__(self, show_logs=False):
         self.show_logs = show_logs
         self.log(f"Init Driver")
-    
-    def show_ctx(self, msg):
-        """
-        Send message to discord and print it
-        :param msg:
-        :return:
-        """
-        self.log(msg)
-        if self.ctx:
-            self.ctx.send(msg)
     
     def log(self, msg):
         """
@@ -46,37 +34,51 @@ class Parser:
         with SB(uc=True, headless=True) as sb:
             self.log(f"Goto {url}")
             sb.driver.get(url)
-            sb.sleep(5)
+            
+            self.log(f"Wait 2.1s")
+            sb.sleep(5.1)
             
             if sb.is_text_visible('Vérification en cours...', '#subButton'):
                 self.log(f"Try new driver")
                 sb.get_new_driver(undetectable=True)
                 sb.driver.get(url)
-                sb.sleep(5)
-
+                self.log(f"Wait 2.1s")
+                sb.sleep(2.1)
+                
                 if sb.is_text_visible('Vérification en cours...', '#subButton'):
                     self.log(f"Try to find iframe")
                     if sb.is_element_visible('iframe[src*="challenge"]'):
                         with sb.frame_switch('iframe[src*="challenge"]'):
                             sb.click("span.mark")
-                            sb.sleep(2)
+                            
+                            self.log(f"Wait 2.1s")
+                            sb.sleep(2.1)
+                            
+            self.log(f"Activate demo mode")
             sb.activate_demo_mode()
             
             self.log(f"Clic on subButton")
             element = sb.find_element(By.ID, "subButton")
             sb.execute_script("arguments[0].click();", element)
             
-            self.log("Try to find link")
-            link = sb.find_element(By.XPATH, XPATH_LINK)
-            if link:
-                href = link.get_attribute('href')
-                self.log(f"Find link {href}")
-                sb.driver.close()
-                return href
-            else:
+            try:
+                self.log("Try to find link")
+                link = sb.find_element(By.XPATH, XPATH_LINK)
+                if link:
+                    href = link.get_attribute('href')
+                    self.log(f"Find link {href}")
+                    sb.driver.close()
+                    return href
+                else:
+                    self.log("Error no link found")
+                    sb.driver.save_screenshot('screen.png')
+                    sb.driver.close()
+                    raise Exception("Error no link found")
+            except Exception as e:
+                self.log(f"Error: {e}")
                 sb.driver.save_screenshot('screen.png')
                 sb.driver.close()
-                self.log(f"Error no link found")
+                raise e
     
     def download_all_series(self, url):
         """
@@ -92,7 +94,6 @@ class Parser:
             wait = WebDriverWait(driver, 10)
             wait.until(presence_of_element_located((By.ID, "main-body")))
         except Exception:
-            self.ctx.send(f'Error: No link found')
             driver.save_screenshot('screen.png')
             return []
         
@@ -101,8 +102,6 @@ class Parser:
         soup = BeautifulSoup(page_source, 'html.parser')
         
         title = soup.find('h1').text.strip().split('»')[1]
-        
-        self.show_ctx(f"Vous voulez télécharger: {title}")
         
         table = soup.find('table', id="DDLLinks")
         trs = table.find_all('tr', class_='link-row')
@@ -113,22 +112,57 @@ class Parser:
             if tr.find_all('td')[1].text == SELECT_PROVIDER:
                 provider_link = tr.find_all('td')[0].find('a').get('href')
                 dl_protect_link = self.dl_protect(provider_link)
-
+                
                 if dl_protect_link:
                     urls.append(dl_protect_link)
         return urls
+    
+    def get_dl_protect_url(self, url):
+        """
+        Get the dl-protect url from wawacity
+        :param url:
+        :return:
+        """
+        driver = Driver(uc=True, headless=True)
+        driver.get(url)
+        self.log(f"Get {url}")
+        
+        try:
+            wait = WebDriverWait(driver, 10)
+            wait.until(presence_of_element_located((By.ID, "main-body")))
+        except Exception:
+            driver.save_screenshot('screen.png')
+            return None, None
+        
+        page_source = driver.page_source
+        
+        soup = BeautifulSoup(page_source, 'html.parser')
+        
+        title = soup.find('h1').text.strip().split('»')[1]
+        
+        table = soup.find('table', id="DDLLinks")
+        trs = table.find_all('tr', class_='link-row')
+        
+        urls = []
+        for tr in trs:
+            if tr.find_all('td')[1].text == SELECT_PROVIDER:
+                provider_link = tr.find_all('td')[0].find('a').get('href')
+                urls.append(provider_link)
+        
+        return title, urls
 
 
 if __name__ == '__main__':
     parser = Parser(show_logs=True)
-
-    parser.dl_protect("https://dl-protect.link/2bd40b83?fn=U2V4IEVkdWNhdGlvbiAtIFNhaXNvbiAzIMOJcGlzb2RlIDEgLSBbVk9TVEZSIEhEXQ%3D%3D&rl=b2")
     
-    all_series_urls = parser.download_all_series("https://www.wawacity.fit/?p=serie&id=12739-sex-education-saison4")
+    # parser.dl_protect("https://dl-protect.link/2bd40b83?fn=U2V4IEVkdWNhdGlvbiAtIFNhaXNvbiAzIMOJcGlzb2RlIDEgLSBbVk9TVEZSIEhEXQ%3D%3D&rl=b2")
+    
+    the_url = "https://www.wawacity.fit/?p=film&id=45008-oppenheimer"
+    
+    all_series_urls = parser.download_all_series(the_url)
     if len(all_series_urls) == 0:
         print("No link found")
     else:
         # list all links into file name result.txt
         with open("result.txt", "w") as f:
             f.write("\n".join(all_series_urls))
-        
