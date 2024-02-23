820b6946a366a5ee506918aa98a6d5435ca75e25
Kevin CUOQ <kevin.cuoq@gmail.com>
2024-02-23 21:07:37 +0100

    Add history for not download same url

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import asyncio
 from slugify import slugify
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 
 from parser import Parser
 
@@ -22,6 +23,7 @@ SELECT_PROVIDER = '1fichier'
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 numbers = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+histories = []
 
 
 def create_embed(item, index, count):
@@ -59,20 +61,20 @@ async def download_by_url(url, type):
         path = os.path.join(DOWNLOAD_PATH, 'Movies')
     elif type == 'serie':
         path = os.path.join(DOWNLOAD_PATH, 'Shows')
-        regex_series = r"(.*).(S[0-9]{1,2})(E[0-9]{1,2}).*(\.[a-z0-9]*)"
+        regex_series = r"(.*).(S[0-9]{1,2})(E[0-9]{1,4}).*(\.[a-z0-9]*)"
         matches = re.match(regex_series, filename)
         if matches:
-          matches = matches.groups()
-          if len(matches) == 4:
-              folder = matches[0].replace('.', ' ')
-              season = f"{folder} - {matches[1]}"
-              path = os.path.join(path, folder)
-              if not os.path.exists(path):
-                  os.makedirs(path)
-              path = os.path.join(path, season)
-              if not os.path.exists(path):
-                  os.makedirs(path)
-              filename = f"{matches[0]}-{matches[1]}{matches[2]}{matches[3]}"
+            matches = matches.groups()
+            if len(matches) == 4:
+                folder = matches[0].replace('.', ' ')
+                season = f"{folder} - {matches[1]}"
+                path = os.path.join(path, folder)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                path = os.path.join(path, season)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                filename = f"{matches[0]}-{matches[1]}{matches[2]}{matches[3]}"
     else:
         print(f'Error: Invalid type')
         return False
@@ -90,7 +92,6 @@ async def download_by_url(url, type):
                     if not chunk:
                         break
                     f.write(chunk)
-
     return filename
 
 
@@ -158,8 +159,8 @@ async def search_wawacity(ctx, query, category, year):
     if category == 'films':
         params['s'] = 'blu-ray_1080p-720p'
     else:
-       params['s'] = 'vostfr-hq'  
-        
+        params['s'] = 'vostfr-hq'
+    
     async with (aiohttp.ClientSession() as session):
         async with session.get(WAWACITY_URL, params=params) as resp:
             if resp.status != 200:
@@ -172,21 +173,33 @@ async def search_wawacity(ctx, query, category, year):
             return data
 
 
-async def download_url_selected(ctx, url, folder, select_provider):
+def is_already_downloaded(url):
+    for history in histories:
+        if history['url'] == url:
+            return True
+    return False
+
+
+async def download_url_selected(ctx, url_selected, folder, select_provider):
     parser = Parser(show_logs=True, select_provider=select_provider)
     
-    mainTitle, urls = parser.get_dl_protect_url(url)
+    main_title, urls = parser.get_dl_protect_url(url_selected)
     if not urls:
         await ctx.send(f'Error: No link found')
         return
     
-    await ctx.send(f'You want to download: {mainTitle}')
+    await ctx.send(f'You want to download: {main_title}')
     
     loop = asyncio.get_event_loop()
-    summary = []
     
     for url in urls:
         print(f'You want to find url from {url}')
+        
+        # check if file already exists in history by url
+        if is_already_downloaded(url):
+            print(f'Error: Already downloaded')
+            continue
+    
         tries = 0
         dl_protect_url = None
         error = None
@@ -223,10 +236,9 @@ async def download_url_selected(ctx, url, folder, select_provider):
         
         await ctx.send(f"{title} - {download_status} - {error}")
         
-        with open(f'summary/{slugify(mainTitle)}.txt', 'w') as f:
-            f.write(f"{title} - {dl_protect_url} - {download_status} - {error}")
-    
-    await ctx.send(f"Summary: {len(summary)} files downloaded")
+        if download_status == 'OK':
+            histories.append({'title': title, 'url': url})
+            pd.DataFrame(histories).to_csv('history.csv', index=False)
 
 
 @bot.command(name='url', help='Download a file by url')
@@ -240,7 +252,7 @@ async def url(ctx, url=None, folder=None):
         return
     
     await download_url_selected(ctx, url, folder, SELECT_PROVIDER)
-    
+
 
 @bot.command(name='search', help='Search a series, films or manga')
 async def search(ctx, query=None, category=None, year=None, count=3, select_provider=SELECT_PROVIDER):
@@ -305,7 +317,10 @@ async def search(ctx, query=None, category=None, year=None, count=3, select_prov
     else:
         await ctx.send('Error: No selection')
 
+
 if __name__ == '__main__':
-    bot.run(TOKEN)
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(download_by_url('https://1fichier.com/?4jpan3acbd740eevl40v&af=3601079', 'serie'))
+    data = pd.read_csv('history.csv')
+    histories = data.to_dict(orient='records')
+    
+    bot.run(
+        TOKEN)  # loop = asyncio.get_event_loop()  # loop.run_until_complete(download_by_url('https://1fichier.com/?4jpan3acbd740eevl40v&af=3601079', 'serie'))
